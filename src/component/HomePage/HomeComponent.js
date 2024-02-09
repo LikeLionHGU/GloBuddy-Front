a4932445fe544fb7f7cb5d84ba6fc0236ec7590f
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-02-10 00:43:13 +0900

    feat: 로그아웃 기능 구현

--- src/component/HomePage/HomeComponent.js
+++ src/component/HomePage/HomeComponent.js
@@ -1,6 +1,16 @@
 import { UserTokenState } from "../../store/atom";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
 import jwtDecode from "jwt-decode";
+import styled from "styled-components";
+
+const LogoutButton = styled.button`
+  border-radius: 8px;
+  font-size: 1rem;
+  border: 1px solid lightgray;
+  color: black;
+  background: white;
+`;
 
 const data = {
   name: "",
@@ -9,18 +19,25 @@ const data = {
 };
 
 function HomeComponent() {
-  const userToken = useRecoilValue(UserTokenState);
+  const [userToken, setUserToken] = useRecoilState(UserTokenState);
   const decodedToken = jwtDecode(userToken);
   data.name = decodedToken.name;
   data.email = decodedToken.email;
   data.picture = decodedToken.picture;
-  console.log(decodedToken);
+  // console.log(decodedToken);
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    setUserToken(null);
+    navigate("/");
+  };
   return (
     <>
       GloBuddy Let's Go~!
       <h1>Welcome, {data.name}</h1>
       <p>Email 정보 : {data.email}</p>
       <img src={data.picture} alt="user profile" />
+      <LogoutButton onClick={handleLogout}>로그아웃</LogoutButton>
     </>
   );
 }
