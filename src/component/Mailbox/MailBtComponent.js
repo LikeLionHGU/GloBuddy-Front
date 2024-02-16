9d16ee1bfaf83726c8e2b5a21bb7e6f51320a665
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-02-17 02:38:23 +0900

    style: 빈 편지함 css 적용

--- src/component/Mailbox/MailBtComponent.js
+++ src/component/Mailbox/MailBtComponent.js
@@ -12,66 +12,66 @@ const MailButton = styled.button`
 // send : IfChecked | true는 확인 / false는 미확인
 // inbox가 1,2이면서 send가 false일때만 응답 받은 편지함에 보이기
 const chatData = [
-  {
-    send: false, // 일단 보낸 편지함
-    inbox: 1,
-    name: "오세훈",
-    type: "국내 더미",
-    gender: "여자 더미",
-    propensity: "차분함 더미",
-    needs: "친구 더미",
-    picture: "더미 사진 링크",
-    text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
-    kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
-  },
-  {
-    send: true, // 일단 보낸 편지함
-    inbox: 2,
-    name: "이한나",
-    type: "국내 더미",
-    gender: "여자 더미",
-    propensity: "차분함 더미",
-    needs: "친구 더미",
-    picture: "더미 사진 링크",
-    text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
-    kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
-  },
-  {
-    send: true, // 일단 보낸 편지함
-    inbox: 0,
-    name: "정정재",
-    type: "국내 더미",
-    gender: "남자 더미",
-    propensity: "차분함 더미",
-    needs: "친구 더미",
-    picture: "더미 사진 링크",
-    text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
-    kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
-  },
-  {
-    send: false, // 일단 보낸 편지함
-    inbox: 0,
-    name: "김민혁",
-    type: "꺄호",
-    gender: "여자 더미",
-    propensity: "차분함 더미",
-    needs: "친구 더미",
-    picture: "더미 사진 링크",
-    text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
-    kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
-  },
-  {
-    send: false, // 일단 보낸 편지함
-    inbox: 2,
-    name: "조예환",
-    type: "뭐",
-    gender: "커피",
-    propensity: "꺄호",
-    needs: "친구 더미",
-    picture: "더미 사진 링크",
-    text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
-    kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
-  },
+  // {
+  //   send: false, // 일단 보낸 편지함
+  //   inbox: 1,
+  //   name: "오세훈",
+  //   type: "국내 더미",
+  //   gender: "여자 더미",
+  //   propensity: "차분함 더미",
+  //   needs: "친구 더미",
+  //   picture: "더미 사진 링크",
+  //   text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
+  //   kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
+  // },
+  // {
+  //   send: true, // 일단 보낸 편지함
+  //   inbox: 2,
+  //   name: "이한나",
+  //   type: "국내 더미",
+  //   gender: "여자 더미",
+  //   propensity: "차분함 더미",
+  //   needs: "친구 더미",
+  //   picture: "더미 사진 링크",
+  //   text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
+  //   kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
+  // },
+  // {
+  //   send: true, // 일단 보낸 편지함
+  //   inbox: 0,
+  //   name: "정정재",
+  //   type: "국내 더미",
+  //   gender: "남자 더미",
+  //   propensity: "차분함 더미",
+  //   needs: "친구 더미",
+  //   picture: "더미 사진 링크",
+  //   text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
+  //   kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
+  // },
+  // {
+  //   send: false, // 일단 보낸 편지함
+  //   inbox: 0,
+  //   name: "김민혁",
+  //   type: "꺄호",
+  //   gender: "여자 더미",
+  //   propensity: "차분함 더미",
+  //   needs: "친구 더미",
+  //   picture: "더미 사진 링크",
+  //   text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
+  //   kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
+  // },
+  // {
+  //   send: false, // 일단 보낸 편지함
+  //   inbox: 2,
+  //   name: "조예환",
+  //   type: "뭐",
+  //   gender: "커피",
+  //   propensity: "꺄호",
+  //   needs: "친구 더미",
+  //   picture: "더미 사진 링크",
+  //   text: "버디를 요청합니다. 이유는 재밌어보여서 입니다.",
+  //   kakao: "https://github.com/LikeLionHGU/GloBuddy-Front/issues/11",
+  // },
 ];
 
 function MailBtComponent() {
--- src/pages/Alarm.js
+++ src/pages/Alarm.js
@@ -4,16 +4,70 @@ import styled from "styled-components";
 import MailAlarmInboxComponent from "../component/Mailbox/MailAlarmInboxComponent";
 import MailAlarmSentItemsComponent from "../component/Mailbox/MailAlarmSentItemsComponent";
 import { Vertical, Horizontal } from "../styles/StyledComponents";
+import { IoMailOutline } from "react-icons/io5";
+import LetterBoxImg from "../img/LetterBox.png";
 
 const BT = styled.button`
-  border: 1px solid lightgray;
-  background: lightgray;
-  cursor: pointer;
+  border: none;
+  border-radius: 15px;
+  background: #ffd4a3;
   width: 150px;
-  height: 50px;
-  margin: 20px;
+  height: 60px;
+  font-size: 18px;
+  font-family: Subtitle3;
+  cursor: pointer;
 `;
-
+const DetailText = styled.p`
+  font-size: 12px;
+  font-family: Subtitle3;
+  color: #948d8d;
+  margin-bottom: 40px;
+`;
+const OrnageBox = styled.div`
+  width: 825px;
+  height: 408px;
+  background-color: #ffe2c1;
+  border-radius: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  margin: 0 40px 45px 150px;
+  padding-top: 70px;
+  font-family: Subtitle2;
+  font-size: 27px;
+  color: #48524b;
+`;
+const LetterText = styled.p`
+  font-family: Subtitle3;
+  font-size: 30px;
+`;
+const WhoText = styled.p`
+  font-family: Subtitle3;
+  font-size: 17px;
+  margin-left: 30px;
+  color: #948d8d;
+  margin-top: 23px;
+`;
+const LetterImg = styled.img`
+  width: 180px;
+  height: 185px;
+  margin-top: 27px;
+  margin-left: auto;
+  margin-right: 25px;
+`;
+export const NoCenterVertical = styled.div`
+  //세로 정렬
+  display: flex;
+  flex-direction: column;
+`;
+export const NoCenterHorizontal = styled.div`
+  //가로 정렬
+  display: flex;
+  align-items: center;
+  margin-left: 150px;
+`;
 function Alarm() {
   const { state } = useLocation();
   const [show, setShow] = useState(true);
@@ -30,21 +84,33 @@ function Alarm() {
   return (
     <Horizontal>
       <Vertical>
-        <BT onClick={handleShowInbox}>Show Inbox</BT>
-        <BT onClick={handleShowSentItems}>Show Sent Items</BT>
+        <BT onClick={handleShowInbox}>Inbox</BT>
+        <DetailText>Who wants to meet me</DetailText>
+        <BT onClick={handleShowSentItems}>Sent Items</BT>
+        <DetailText>Who I want to meet</DetailText>
       </Vertical>
-      <Vertical>
-        {isEmptyInbox ? (
-          <div>
-            Your Letterbox is empty! Meet more global buddies from the buddy
-            request !
-          </div>
-        ) : show ? (
-          <MailAlarmInboxComponent chatData={state} />
-        ) : (
-          <MailAlarmSentItemsComponent chatData={state} />
-        )}
-      </Vertical>
+      <NoCenterVertical>
+        <NoCenterHorizontal>
+          <IoMailOutline
+            style={{ width: "47px", height: "35px", color: "#FF9571" }}
+          />
+          <LetterText>Letter</LetterText>
+          <WhoText>Who wants to meet me</WhoText>
+        </NoCenterHorizontal>
+        <Vertical>
+          {isEmptyInbox ? (
+            <OrnageBox>
+              Your Letterbox is empty!
+              <br /> Meet more global buddies from the buddy request !
+              <LetterImg src={LetterBoxImg} alt="letterbox" />
+            </OrnageBox>
+          ) : show ? (
+            <MailAlarmInboxComponent chatData={state} />
+          ) : (
+            <MailAlarmSentItemsComponent chatData={state} />
+          )}
+        </Vertical>
+      </NoCenterVertical>
     </Horizontal>
   );
 }
