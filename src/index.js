1c0723f3261d4e8d42db23df2e216e3a2cae4ea3
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-02-20 20:32:24 +0900

    style: 로그인 전 메인 css 수정

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import styled from "styled-components";
 import App from "./App";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 const Div = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  width: 98vw;
+  width: 100vw;
   /* height: 98vh; */
   align-items: center;
 `;
@@ -15,6 +16,7 @@ const Div = styled.div`
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Div>
       <App />
     </Div>
