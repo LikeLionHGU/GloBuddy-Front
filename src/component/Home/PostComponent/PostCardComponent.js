40b29ccfb3e8f2270085c7263b8f95679e8dcd9c
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-02-21 00:14:48 +0900

    feat: buddy 신청 모달 세분화

--- src/component/Home/PostComponent/PostCardComponent.js
+++ src/component/Home/PostComponent/PostCardComponent.js
@@ -1,56 +1,31 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import PostCardIconImg from "../../../img/PostCardIcon.png";
 import PostCardImg from "../../../img/PostCard.png";
+import PostCardModal from "./PostCardModal";
 
-const Popup = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 300px;
-  padding: 20px;
-  background: white;
-  border: 1px solid lightgray;
-  border-radius: 8px;
-`;
-
-const RequestForm = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 20px;
-`;
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  font-size: 1rem;
-  background: none;
-  border: none;
-  cursor: pointer;
-`;
-
-const SubmitButton = styled.button`
-  margin-top: 10px;
-  border-radius: 8px;
-  font-size: 1rem;
-  border: 1px solid lightgray;
-  color: black;
-  background: white;
-`;
 const CardImg = styled.img`
-  width: 100px;
-  display: flex;
-  justify-content: center;
+  width: 370px;
+  height: 158px;
+  margin-left: -5px;
+  margin-top: 10px;
+  margin-bottom: 15px;
 `;
-
-const Font = styled.p`
-  /* font-size: 15px; */
+const TitleText = styled.p`
+  font-size: 30px;
   margin: 0;
   font-family: Body2; // Global Style 적용해줘서 이렇게 사용 가능
 `;
-
+const Font = styled.p`
+  font-size: 15px;
+  /* margin: 0; */
+  font-family: Body2; // Global Style 적용해줘서 이렇게 사용 가능
+`;
+const Font2 = styled.p`
+  font-size: 15px;
+  margin-left: 70px;
+  font-family: Body2; // Global Style 적용해줘서 이렇게 사용 가능
+`;
 const Cards = styled.div`
   width: 100%;
   height: 100%;
@@ -59,8 +34,8 @@ const Cards = styled.div`
   transform-style: preserve-3d;
 `;
 const Flip = styled.div`
-  width: 400px;
-  height: 400px;
+  width: 360px;
+  height: 352px;
   position: relative;
 
   perspective: 1100px;
@@ -69,40 +44,77 @@ const Flip = styled.div`
   }
   margin: 4rem;
 `;
-
 const CardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 `;
-
+const Horizontal = styled.div`
+  //가로 정렬
+  display: flex;
+  align-items: center;
+  /* justify-content: center; */
+`;
 const Front = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
-  border-radius: 8px;
+  border-radius: 15px;
   backface-visibility: hidden;
   background: white;
   border: 1px solid black;
+  padding: 20px;
 `;
-
 const Back = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
   backface-visibility: hidden;
   background: white;
-  border-radius: 8px;
+  border-radius: 15px;
   border: 1px solid black;
   transform: rotateY(180deg);
+  padding: 20px;
+`;
+const TagBox = styled.button`
+  width: 70px;
+  height: 30px;
+  border-radius: 20px;
+  border: none;
+  background: ${({ color }) => color};
+  margin-left: 10px;
+  font-size: 13px;
+`;
+const IconImg = styled.img`
+  width: 13px;
+  height: 12px;
+`;
+const BackTextBox = styled.div`
+  width: 360px;
+  height: 219px;
+  background-color: #ffe2c1;
+  margin-top: 16px;
+  font-size: 18px;
+  font-family: Subtitle3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 3px;
+`;
+const RequestButton = styled.button`
+  width: 120px;
+  height: 40px;
+  background-color: #ffa640;
+  border: none;
+  margin-top: 25px;
+  margin-left: 33%;
+  font-size: 20px;
+  font-family: Subtitle2;
+  cursor: pointer;
 `;
 
 function PostCardComponent({ postData }) {
   const [showPopup, setShowPopup] = useState(false);
-  const [requestText, setRequestText] = useState("");
-  const [kakaoLink, setKakaoLink] = useState("");
-  const [isLinkValid, setIsLinkValid] = useState(true);
-  const [isRequestSubmitted, setIsRequestSubmitted] = useState(false);
 
   const handleApplyClick = () => {
     setShowPopup(true);
@@ -110,31 +122,6 @@ function PostCardComponent({ postData }) {
 
   const closePopup = () => {
     setShowPopup(false);
-    setRequestText("");
-    setKakaoLink("");
-    setIsLinkValid(true);
-    setIsRequestSubmitted(false);
-  };
-
-  const handleRequestTextChange = (e) => {
-    setRequestText(e.target.value);
-  };
-
-  const handleKakaoLinkChange = (e) => {
-    setKakaoLink(e.target.value);
-    setIsLinkValid(true);
-  };
-
-  const handleSubmit = () => {
-    console.log("요청 내용:", requestText);
-    console.log("카카오톡 링크:", kakaoLink);
-
-    if (kakaoLink.startsWith("https://open.kakao.com/")) {
-      setIsRequestSubmitted(true);
-      setShowPopup(false);
-    } else {
-      setIsLinkValid(false);
-    }
   };
 
   return (
@@ -145,57 +132,36 @@ function PostCardComponent({ postData }) {
             {" "}
             <Cards>
               <Front>
-                <h3>
-                  TITLE:
-                  {data.title}
-                </h3>
+                <TitleText>TITLE: {data.title}</TitleText>
                 <CardImg src={PostCardImg} alt="defalut" />
-                <Font>
-                  Name: {data.name} / 국내|국제 {data.type}
-                </Font>
-                <Font>{data.gender}</Font>
-                <Font>Needs: {data.needs}</Font>
+                <Horizontal>
+                  <Font>Name: {data.name}</Font>
+                  <Font2>
+                    | {data.gender} | {data.type}
+                  </Font2>
+                  <IconImg src={PostCardIconImg} alt="icon" />
+                </Horizontal>
+                <Horizontal>
+                  <Font>MBTI:</Font>
+                  <TagBox color={data.color}> {data.mbti}</TagBox>
+                </Horizontal>
+                <Horizontal>
+                  <Font>Needs: {data.needs}</Font>
+                  <TagBox color={data.color}> 추후 연결 </TagBox>
+                </Horizontal>
               </Front>
-
               <Back>
-                <button onClick={handleApplyClick}>신청하기</button>
+                <TitleText>TITLE: {data.title}</TitleText>
+                <BackTextBox>{data.content}</BackTextBox>
+                <RequestButton onClick={handleApplyClick}>
+                  Request
+                </RequestButton>
               </Back>
             </Cards>
           </Flip>
         ))}
       </CardContainer>
-      {showPopup && (
-        <Popup>
-          <CloseButton onClick={closePopup}>X</CloseButton>
-          <h2>버디 요청하기</h2>
-          <p>텍스트를 입력하세요. ex) 가벼운 인사말을 적어보세요.</p>
-          <textarea
-            value={requestText}
-            onChange={handleRequestTextChange}
-            rows={4}
-            cols={30}
-          />
-          <p>오픈 카카오톡 링크를 삽입하세요.</p>
-          <input
-            type="text"
-            value={kakaoLink}
-            onChange={handleKakaoLinkChange}
-          />
-          {!isLinkValid && (
-            <p style={{ color: "red" }}>형식이 바르지 않습니다!</p>
-          )}
-          <RequestForm>
-            <SubmitButton onClick={handleSubmit}>보내기</SubmitButton>
-            <button onClick={closePopup}>취소</button>
-          </RequestForm>
-        </Popup>
-      )}
-      {isRequestSubmitted && (
-        <Popup>
-          <h2>완료되었습니다!</h2>
-          <button onClick={closePopup}>닫기</button>
-        </Popup>
-      )}
+      {showPopup && <PostCardModal closePopup={closePopup} />}
     </div>
   );
 }
